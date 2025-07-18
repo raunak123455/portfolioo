eeab995d7e3cd4f89a23573b8f260ac08bdbd7ab
raunak123455 <91775838+raunak123455@users.noreply.github.com>
2025-07-18 18:04:36 +0530

    Update Projects.jsx

--- src/Components/Projects/Projects.jsx
+++ src/Components/Projects/Projects.jsx
@@ -14,33 +14,36 @@ const Projects = () => {
       <h1 className="text-2xl md:text-4xl text-white font-bold">Projects</h1>
       <div className="py-12 px-8 flex flex-wrap gap-5">
           <ProjectCard
-          title="Job Finder"
-          main="This fullstack job finder platform is designed to simplify and streamline the job search and hiring process. Built with a modern and scalable tech stack, it provides seamless functionality for both job seekers and employers."
-          demo="https://naukri-ten.vercel.app/"
-          source="https://github.com/raunak123455/Naukri"
-          img={pj5}
-        />
-        <ProjectCard
-          title="Finance Dashboard"
-          main="Personal Investment Tracking Web App Designed and developed a responsive finance dashboard using modern web technologies to track and visualize personal investment data"
-          demo="https://finance-dashboard-rose.vercel.app/"
-          source="https://github.com/raunak123455/Finance-dashboard"
-          img={pj2}
-        />
-        <ProjectCard
           title="Project Management System"
           main="This app helps individuals and teams organize tasks, track progress, and collaborate seamlessly. With features to create, assign, and update tasks, it simplifies workflows and keeps everyone aligned "
           demo="https://task-finall.vercel.app/"
           source="https://github.com/raunak123455/Task-final/tree/main"
           img={pj}
         />
-        <ProjectCard
+         <ProjectCard
+          title="Finance Dashboard"
+          main="Personal Investment Tracking Web App Designed and developed a responsive finance dashboard using modern web technologies to track and visualize personal investment data"
+          demo="https://finance-dashboard-rose.vercel.app/"
+          source="https://github.com/raunak123455/Finance-dashboard"
+          img={pj2}
+        />
+          <ProjectCard
           title="DEFI Marketplace"
           main="This is a DeFi marketplace that allows users to create tokens, launch ICOs, and seamlessly trade or purchase tokens in a decentralized environment "
           demo="https://defi-marketplacee.vercel.app/"
           source="https://github.com/raunak123455/DEFI-MARKETPLACE-2"
           img={pj3}
         />
+          <ProjectCard
+          title="Job Finder"
+          main="This fullstack job finder platform is designed to simplify and streamline the job search and hiring process. Built with a modern and scalable tech stack, it provides seamless functionality for both job seekers and employers."
+          demo="https://naukri-ten.vercel.app/"
+          source="https://github.com/raunak123455/Naukri"
+          img={pj5}
+        />
+       
+      
+      
          <ProjectCard
           title="URL shortener"
           main="This is a full-stack application that shortens long URLs, tracks click analytics, and handles redirection using React, Node.js, Express, and MongoDB. Includes RESTful APIs, unique URL generation, and click tracking"
